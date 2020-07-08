843f31834c180246ecb9d4be4259e88e3ba9a401
Keith Brantley <Keith.M.Brantley@gmail.com>
2020-07-08 15:03:46 -0700

    completed hello flask

--- python_stack/flask/flask_fundamentals/hello_flask/hello.py
+++ python_stack/flask/flask_fundamentals/hello_flask/hello.py
@@ -3,5 +3,21 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/success')
+def success():
+    return 'success'
+
+@app.route('/hello/<name>')
+def hello(name):
+    print(name)
+    return 'Hello, ' + name
+
+@app.route('/users/<username>/<id>')
+def show_user_profile(username, id):
+    print(username)
+    print(id)
+    return 'username: ' + username + ', id: ' + id
+
 if __name__=="__main__":
     app.run(debug=True)
