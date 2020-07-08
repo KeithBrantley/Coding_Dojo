c27ebf41408151b323bf126038f2c3a5e79739cd
Keith Brantley <Keith.M.Brantley@gmail.com>
2020-07-08 14:38:53 -0700

    moved folder into my_environments

--- python_stack/flask/flask_fundamentals/hello_flask/hello.py
+++ python_stack/flask/flask_fundamentals/hello_flask/hello.py
@@ -1,7 +1,7 @@
-from flask import Flask  # Import Flask to allow us to create our app
-app = Flask(__name__)    # Create a new instance of the Flask class called "app"
-@app.route('/')          # The "@" decorator associates this route with the function immediately following
+from flask import Flask
+app = Flask(__name__)
+@app.route('/')
 def hello_world():
-    return 'Hello World!'  # Return the string 'Hello World!' as a response
-if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
-    app.run(debug=True)    # Run the app in debug mode.
+    return 'Hello World!'
+if __name__=="__main__":
+    app.run(debug=True)
