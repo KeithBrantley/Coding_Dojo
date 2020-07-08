96b85758dd7f53b834b396686be0ca85b139c193
Keith Brantley <Keith.M.Brantley@gmail.com>
2020-07-08 15:21:23 -0700

    hello world.py complete

--- python_stack/flask/flask_fundamentals/hello_world/hello_world.py
+++ python_stack/flask/flask_fundamentals/hello_world/hello_world.py
@@ -4,5 +4,18 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/dojo')
+def dojo():
+    return 'dojo'
+
+@app.route('/say/<name>')
+def say_name(name):
+    return 'Hi, ' + name
+
+@app.route('/repeat/<num>/<name>')
+def repeat(num, name):
+    return name * int(num)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
