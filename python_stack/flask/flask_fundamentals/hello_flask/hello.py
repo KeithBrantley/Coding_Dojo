5079ce24427356eb2dcedb70946f2b2d65ebe0fc
Keith Brantley <Keith.M.Brantley@gmail.com>
2020-07-08 15:45:19 -0700

    lesson complete

--- python_stack/flask/flask_fundamentals/hello_flask/hello.py
+++ python_stack/flask/flask_fundamentals/hello_flask/hello.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html', phrase="hello", times=5)
 
 @app.route('/success')
 def success():
