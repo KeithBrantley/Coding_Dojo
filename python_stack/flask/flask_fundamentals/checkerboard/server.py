eec62ebfa808c66d55d2154b43d878bb680dd2e3
Keith Brantley <Keith.M.Brantley@gmail.com>
2020-07-08 21:37:53 -0700

    lesson complete

--- python_stack/flask/flask_fundamentals/checkerboard/server.py
+++ python_stack/flask/flask_fundamentals/checkerboard/server.py
@@ -4,5 +4,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/<x>')
+def custom(x):
+    x = int(x)
+    return render_template('custom.html', x = x)
+
 if __name__ == "__main__":
     app.run(debug=True)
