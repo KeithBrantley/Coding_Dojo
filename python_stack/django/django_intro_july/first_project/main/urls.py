750e5fc2baeb17eba8f301b95ab3cf75ee78a6e2
Keith Brantley <Keith.M.Brantley@gmail.com>
2020-07-09 12:13:54 -0700

    assignment complete minus bonus

--- python_stack/django/django_intro_july/first_project/main/urls.py
+++ python_stack/django/django_intro_july/first_project/main/urls.py
@@ -2,5 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.root),
+    path('blogs', views.index),
+    path('blogs/new', views.new),
+    path('blogs/create', views.create),
+    path('blogs/<int:number>', views.show),
+    path('blogs/<int:number>/edit', views.edit),
+    path('blogs/<int:number>/delete', views.destroy),
 ]
--- python_stack/django/django_intro_july/first_project/main/views.py
+++ python_stack/django/django_intro_july/first_project/main/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import HttpResponse, redirect
 
 # Create your views here.
+def root(request):
+    return HttpResponse("This is the root")
+
 def index(request):
-    return HttpResponse('This is the root')
+    return HttpResponse("placeholder to later display a list of all blogs")
+
+def new(request):
+    return HttpResponse("placeholder to display a new form to create a new blog")
+
+def create(request):
+    return redirect('/blogs')
+
+def show(request, number):
+    return HttpResponse(f"placeholder to display blog number: {number}")
+
+def edit(request, number):
+    return HttpResponse(f"placeholder to edit blog {number}")
+
+def destroy(request, number):
+    return redirect('/blogs')
