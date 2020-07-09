01b95371a7c4b6e3ec3e9d7b7258a15c4dc39e3c
Keith Brantley <Keith.M.Brantley@gmail.com>
2020-07-09 10:47:53 -0700

    django env

---
+++ python_stack/my_environments/djangoEnv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\keith\documents\codingdojo\python_stack\my_environments\djangoenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
