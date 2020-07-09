77ea6a4bceedae94fd1495a8e64f73a679d6fafc
Keith Brantley <Keith.M.Brantley@gmail.com>
2020-07-08 18:51:42 -0700

    lesson complete

--- python_stack/flask/flask_fundamentals/playground/playground.py
+++ python_stack/flask/flask_fundamentals/playground/playground.py
@@ -4,5 +4,16 @@ app = Flask(__name__)
 def play():
     return render_template('index.html')
 
+@app.route('/play/<num>')
+def boxes(num):
+    num = int(num)
+    return render_template('boxes.html', num = num)
+
+@app.route('/play/<num>/<color>')
+def color(num, color):
+    num = int(num)
+    color = color
+    return render_template('color.html', num = num, color = color)
+
 if __name__ == "__main__":
     app.run(debug=True)
