b8d1854fe663bc30684e158f62b8a9e04bbea06d
Keith Brantley <Keith.M.Brantley@gmail.com>
2020-07-09 22:13:52 -0700

    not complete

--- python_stack/django/django_intro_july/form_test/form_app/urls.py
+++ python_stack/django/django_intro_july/form_test/form_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('users',views.create_user),
 ]
--- python_stack/django/django_intro_july/form_test/form_app/views.py
+++ python_stack/django/django_intro_july/form_test/form_app/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def create_user(request):
+    print("Got Post Info....................")
+    print(request.POST)
+    return render(request,"index.html")
