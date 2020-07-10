960580a3dda0917d30f498ad2e68e87460b1161c
Keith Brantley <Keith.M.Brantley@gmail.com>
2020-07-10 11:40:39 -0700

    added last name input

--- python_stack/django/django_intro_july/form_test/form_app/views.py
+++ python_stack/django/django_intro_july/form_test/form_app/views.py
@@ -6,5 +6,12 @@ def index(request):
 
 def create_user(request):
     print("Got Post Info....................")
-    print(request.POST)
-    return render(request,"index.html")
+    first_name_from_form = request.POST['first_name']
+    last_name_from_form = request.POST['last_name']
+    email_from_form = request.POST['email']
+    context = {
+        "first_name_on_template" : first_name_from_form,
+        "last_name_on_template" : last_name_from_form,
+        "email_on_template" : email_from_form
+    }
+    return render(request,"show.html", context)
