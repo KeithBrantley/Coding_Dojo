15036e789100adf5030018372cd9938ca35e8f2d
Keith Brantley <Keith.M.Brantley@gmail.com>
2020-07-10 11:52:18 -0700

    section complete

--- python_stack/django/django_intro_july/form_test/form_app/urls.py
+++ python_stack/django/django_intro_july/form_test/form_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('users',views.create_user),
+    path('success', views.success),
 ]
--- python_stack/django/django_intro_july/form_test/form_app/views.py
+++ python_stack/django/django_intro_july/form_test/form_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
@@ -14,4 +14,7 @@ def create_user(request):
         "last_name_on_template" : last_name_from_form,
         "email_on_template" : email_from_form
     }
-    return render(request,"show.html", context)
+    return redirect('/success')
+
+def success(request):
+    return render(request, "success.html")
