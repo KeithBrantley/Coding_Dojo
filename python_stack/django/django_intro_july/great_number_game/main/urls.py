7e1f3bbe9d3ccb03cb32ff8c3c9b1192c904ad75
Keith Brantley <Keith.M.Brantley@gmail.com>
2020-07-14 12:01:25 -0700

    working on functionality

--- python_stack/django/django_intro_july/great_number_game/main/urls.py
+++ python_stack/django/django_intro_july/great_number_game/main/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('/guess', views.guess_num),
 ]
--- python_stack/django/django_intro_july/great_number_game/main/views.py
+++ python_stack/django/django_intro_july/great_number_game/main/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+import random
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def guess_num(request):
+    number = request.POST['number']
+    rand_num = random.randint(1, 100)
+    if number < rand_num:
+        
+    print(rand_num)
