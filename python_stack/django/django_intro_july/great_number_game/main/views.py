92bf275fd42c330bdfd9d17f60732318731965af
Keith Brantley <Keith.M.Brantley@gmail.com>
2020-07-14 14:53:35 -0700

    not complete

--- python_stack/django/django_intro_july/great_number_game/main/views.py
+++ python_stack/django/django_intro_july/great_number_game/main/views.py
@@ -6,8 +6,4 @@ def index(request):
     return render(request, 'index.html')
 
 def guess_num(request):
-    number = request.POST['number']
-    rand_num = random.randint(1, 100)
-    if number < rand_num:
-        
-    print(rand_num)
+    pass
