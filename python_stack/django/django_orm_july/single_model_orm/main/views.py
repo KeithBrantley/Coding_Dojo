e6b6203c8f050da7cbda5a9f68fde85831a28ab7
Keith Brantley <Keith.M.Brantley@gmail.com>
2020-07-15 23:34:24 -0700

    users render on page

--- python_stack/django/django_orm_july/single_model_orm/main/views.py
+++ python_stack/django/django_orm_july/single_model_orm/main/views.py
@@ -4,7 +4,8 @@ from .models import User
 # Create your views here.
 
 def index(request):
+    print(User.objects.all())
     context ={
         "all_users": User.objects.all()
     }
-    return render(request, 'index.html')
+    return render(request, 'index.html', context)
