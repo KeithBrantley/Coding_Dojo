4ad8c29ebca3160fb067b044f453fb10322d808f
Keith Brantley <Keith.M.Brantley@gmail.com>
2020-07-15 23:08:49 -0700

    added form to html

--- python_stack/django/django_orm_july/single_model_orm/main/views.py
+++ python_stack/django/django_orm_july/single_model_orm/main/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from .models import User
 
 # Create your views here.
 
 def index(request):
+    context ={
+        "all_users": User.objects.all()
+    }
     return render(request, 'index.html')
