6f9f1b34ab21fd8cf7ad9539b0fde5e7314e292d
Keith Brantley <Keith.M.Brantley@gmail.com>
2020-07-15 23:50:15 -0700

    app complete

--- python_stack/django/django_orm_july/single_model_orm/main/views.py
+++ python_stack/django/django_orm_july/single_model_orm/main/views.py
@@ -1,11 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import User
 
 # Create your views here.
 
+
 def index(request):
     print(User.objects.all())
-    context ={
+    context = {
         "all_users": User.objects.all()
     }
     return render(request, 'index.html', context)
+
+
+def submit(request):
+    print(request.POST)
+    User.objects.create(
+    first_name=request.POST['first_name'], 
+    last_name=request.POST['last_name'], 
+    email=request.POST['email'], 
+    age=request.POST['age'])
+    return redirect('/')
