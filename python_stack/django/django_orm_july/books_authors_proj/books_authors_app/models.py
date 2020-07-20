967dc7c7032a782bb38e42d83a3354ce7117b0c6
Keith Brantley <keith.m.brantley@gmail.com>
2020-07-19 22:46:18 -0700

    books authors shell complete

--- python_stack/django/django_orm_july/books_authors_proj/books_authors_app/models.py
+++ python_stack/django/django_orm_july/books_authors_proj/books_authors_app/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 
 # Create your models here.
-
 class Book(models.Model):
     title = models.CharField(max_length=255)
     desc = models.TextField()
@@ -9,13 +8,17 @@ class Book(models.Model):
     updated_at = models.DateField(auto_now=True)
 
     def __str__(self):
-        return f"{self.id} {self.title} {self.desc}" 
+        return f"{self.title} {self.desc}" 
 
 class Author(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
+    notes = models.TextField(default=True)
+    books = models.ManyToManyField(Book, related_name="authors")
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateField(auto_now=True)
 
     def __str__(self):
-        return f"{self.id} {self.first_name} {self.last_name}"
+        return f"{self.first_name} {self.last_name}"
+        
+
