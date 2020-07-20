e0ba61979b3cb7b5f2decb92376258ae662f8bb2
Keith Brantley <keith.m.brantley@gmail.com>
2020-07-19 21:00:29 -0700

    added book and author class

--- python_stack/django/django_orm_july/books_authors_proj/books_authors_app/models.py
+++ python_stack/django/django_orm_july/books_authors_proj/books_authors_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    desc = models.TextField()
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.id} {self.title} {self.desc}" 
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.id} {self.first_name} {self.last_name}"
