6f2ba3b38b989abbe5d632be2af5f17457cd0637
Keith Brantley <keith.m.brantley@gmail.com>
2020-07-21 15:38:50 -0700

    updated commit

--- python_stack/django/django_full_stack_july/semi_restful_tv_shows/main/urls.py
+++ python_stack/django/django_full_stack_july/semi_restful_tv_shows/main/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('shows/new', views.index),
+    path('add_a_show', views.add_a_show),
 ]
--- python_stack/django/django_full_stack_july/semi_restful_tv_shows/main/views.py
+++ python_stack/django/django_full_stack_july/semi_restful_tv_shows/main/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render
+from .models import Show
 
 # Create your views here.
+
+
 def index(request):
     return render(request, 'add_show.html')
+
+
+def add_a_show(request):
+    Show.objects.create(
+        title=request.POST['title'],
+        network=request.POST['network'],
+        release_date=request.POST['release_date'],
+        description=request.POST['description']
+    )
+    return render(request, 'tv_show.html')
+
+def view_show()
