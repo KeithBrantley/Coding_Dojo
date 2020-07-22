4d78804df60e2ee4606db170dcc8e7eb072a649a
Keith Brantley <keith.m.brantley@gmail.com>
2020-07-21 20:40:32 -0700

    made migrations

--- python_stack/django/django_orm_july/books_authors/main/models.py
+++ python_stack/django/django_orm_july/books_authors/main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    notes = models.CharField(max_length=50)
+    books = models.ManyToManyField(Book, related_name="authors")
+    creaated_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
