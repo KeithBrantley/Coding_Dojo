4cc856e29510e78116bd12e7085d7fae65bfcc7b
Keith Brantley <keith.m.brantley@gmail.com>
2020-07-22 15:50:00 -0700

    added userManager

--- python_stack/django/django_full_stack_july/login_reg/main/models.py
+++ python_stack/django/django_full_stack_july/login_reg/main/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class UserManager(models.Manager):
+    def registration_validator(self, post_data):
+        errors = {}
+        if len(post_data['first_name']) < 3:
+            errors['first_name'] = 'You must have 3 characters in your name!'
+
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    email = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
