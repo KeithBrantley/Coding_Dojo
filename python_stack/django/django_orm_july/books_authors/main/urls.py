892fae31d88ca6989f55a66e50245d7721dd9313
Keith Brantley <keith.m.brantley@gmail.com>
2020-07-21 22:00:54 -0700

    added authors html page

--- python_stack/django/django_orm_july/books_authors/main/urls.py
+++ python_stack/django/django_orm_july/books_authors/main/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('process_book', views.create_book),
+    path('book/<int:book_id>',views.one_book),
+    path('author/<int:author_id>',views.one_author),
+    path('authors', views.authors),
+    path('process_author', views.create_author),
 ]
--- python_stack/django/django_orm_july/books_authors/main/views.py
+++ python_stack/django/django_orm_july/books_authors/main/views.py
@@ -1,6 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Book, Author
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    context = {
+        'all_books': Book.objects.all()
+    }
+    return render(request, 'index.html', context)
+
+def create_book(request):
+    Book.objects.create(
+        title=request.POST['title'],
+        description=request.POST['description']
+    )
+    return redirect('/')
+
+def one_book(request, book_id):
+    one_book = Book.objects.get(id=book_id)
+    context = {
+        "one_book": one_book
+    }
+    return render(request, 'one_book.html', context)
+
+    # Author
+
+def authors(request):
+    context = {
+        'all_authors': Author.objects.all()
+    }
+    return render(request, 'authors.html', context)
+
+def create_author(request):
+    Author.objects.create(
+        first_name=request.POST['first_name'],
+        last_name=request.POST['last_name'],
+        notes=request.POST['notes']
+    )
+    return redirect('/authors')
+
+def one_author(request, author_id):
+    one_author = Author.objects.get(id=author_id)
+    context = {
+        "one_author": one_author
+    }
+    return render(request, 'one_author.html', context)
