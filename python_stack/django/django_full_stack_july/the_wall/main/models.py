5e9dea4cc14f07a8888f5a82ff78101edb0f7db2
Keith Brantley <keith.m.brantley@gmail.com>
2020-07-23 06:53:14 -0700

    made migrations

--- python_stack/django/django_full_stack_july/the_wall/main/models.py
+++ python_stack/django/django_full_stack_july/the_wall/main/models.py
@@ -7,10 +7,10 @@ class User(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.CharField(max_length=50)
-    password = models.CharField(max=50)
+    password = models.CharField(max_length=50)
     created_at = models.DateField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    
+
 class Message(models.Model):
     user = models.ForeignKey(User, related_name="messages", on_delete=models.CASCADE)
     message = models.TextField()
