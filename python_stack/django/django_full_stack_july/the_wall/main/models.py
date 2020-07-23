412c7cbccb5a42a0f4d6b5f5cb5b3db6ff613c57
Keith Brantley <keith.m.brantley@gmail.com>
2020-07-23 06:51:20 -0700

    completed models/before migration

--- python_stack/django/django_full_stack_july/the_wall/main/models.py
+++ python_stack/django/django_full_stack_july/the_wall/main/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    password = models.CharField(max=50)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+class Message(models.Model):
+    user = models.ForeignKey(User, related_name="messages", on_delete=models.CASCADE)
+    message = models.TextField()
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Comment(models.Model):
+    message = models.ForeignKey(Message, related_name="comments", on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name="comment", on_delete=models.CASCADE)
+    message = models.TextField()
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
